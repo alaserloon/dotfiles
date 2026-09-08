455cb0e0759e3c3aa65c78049b676d06848d855c
alaserloon <troy.osman@proton.me>
2026-09-08 13:59:28 -0500

    Switched zen-browser flake

--- flake.nix
+++ flake.nix
@@ -29,8 +29,12 @@
     };
 
     zen-browser = {
-      url = "github:youwen5/zen-browser-flake";
-      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:0xc000022070/zen-browser-flake";
+      # url = "github:youwen5/zen-browser-flake";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        home-manager.follows = "home-manager";
+      };
     };
 
     spicetify-nix = {
